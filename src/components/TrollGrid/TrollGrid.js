c58821b99cc7eaa9315b92f2c5f853acaac3759e
DrZin <61010707@kmitl.ac.th>
2019-06-30 02:26:08 +0700

    commit use class base

--- src/components/TrollGrid/TrollGrid.js
+++ src/components/TrollGrid/TrollGrid.js
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
-import tileData from "./titleData";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -35,18 +34,63 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function TrollGrid() {
-  const classes = useStyles();
+export default class TrollGrid extends React.Component {
+  // const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <GridList cellHeight={160} className={classes.gridList} cols={3}>
-        {tileData.map(tile => (
-          <GridListTile key={tile.img}>
-            <img src={tile.img} alt={"img"} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
-  );
+  // return (
+  //   <div className={classes.root}>
+  //     <GridList cellHeight={160} className={classes.gridList} cols={3}>
+  //       {tileData.map(tile => (
+  //         <GridListTile key={tile.img}>
+  //           <img src={tile.img} alt={"img"} />
+  //         </GridListTile>
+  //       ))}
+  //     </GridList>
+  //   </div>
+  // );
+
+  render() {
+    let array = [];
+    const fetch = () => {
+      for (let i = 1; i < 10; i++) {
+        array.push(i);
+      }
+      console.log(array);
+    };
+
+    fetch();
+    return (
+      <div
+        className={useStyles.root}
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          overflow: "hidden",
+          backgroundColor: "lightgray"
+        }}
+      >
+        <GridList
+          cellHeight={160}
+          className={useStyles.gridList}
+          style={{
+            width: " 500",
+            height: "400"
+          }}
+          cols={3}
+        >
+          {array.map(imgC => {
+            return (
+              <GridListTile key={imgC}>
+                <img
+                  src={`http://02a7dd8e.ngrok.io/user/image/${imgC}`}
+                  alt=""
+                />
+              </GridListTile>
+            );
+          })}
+        </GridList>
+      </div>
+    );
+  }
 }
