ee286a827f16c78084beec03b3a3dfb3ea730324
DrZin <61010707@kmitl.ac.th>
2019-06-29 20:24:04 +0700

    commit axios

--- src/components/TrollFooter/TrollFooter.js
+++ src/components/TrollFooter/TrollFooter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Axios, { post } from "axios";
 import { Toolbar, Button } from "react95";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,7 +9,20 @@ import {
   faHeart,
   faUserAlt
 } from "@fortawesome/free-solid-svg-icons";
+
 const TrollFooter = () => {
+  const handleChange = e => {
+    let files = e.target.files;
+    console.log(files);
+    let reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = e => {
+      console.log("img data", e.target.result);
+      const url = "http://1779896c.ngrok.io/user/image";
+      const formData = { imageFile: e.target.result };
+      return Axios.post(url, formData);
+    };
+  };
   return (
     <div>
       <Toolbar style={{ justifyContent: "space-between" }}>
@@ -20,10 +34,23 @@ const TrollFooter = () => {
           {"  "}
           <FontAwesomeIcon icon={faSearch} />
         </Button>
-        <Button className="footerButton">
-          {"  "}
+        <Button
+          className="footerButton"
+          type="file"
+          name="file"
+          onClick={() => {
+            document.getElementById("file-input").click();
+          }}
+        >
           <FontAwesomeIcon icon={faCamera} />
         </Button>
+        <input
+          id="file-input"
+          type="file"
+          name="file"
+          style={{ display: "none" }}
+          onChange={handleChange}
+        />
         <Button className="footerButton">
           {"  "}
           <FontAwesomeIcon icon={faHeart} />
